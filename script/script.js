f3bd4ab3777450fd528e518651bb25b00ad08b93
Esmond Kan <ek.public@protonmail.com>
2016-04-08 16:15:54 -0700

    feat: Add navigation bar

---
+++ script/script.js
@@ -0,0 +1,24 @@
+/* Declare global variable and set initial state */
+var menuState = false;
+
+  function menuToggle() {
+    var menu = document.getElementById("menu");
+
+    if (menuState)
+        menu.style.maxHeight = "0";
+    else {
+        menu.style['transition'] = 'max-height 0.5s ease-in-out';
+        menu.style.maxHeight = "500px";
+    }
+    menuState = !menuState;
+  }
+
+  function checkMedia() {
+    var mq = window.matchMedia("(min-width: 641px");
+    var menu = document.getElementById("menu");
+
+    if (mq.matches)
+        menu.style.maxHeight = "500px";
+    else
+        menu.style.maxHeight = "0";
+  }
