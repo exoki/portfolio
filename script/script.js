4975f7630b36597aeb24c783a4f469e65ed4ef9a
Esmond Kan <ek.public@protonmail.com>
2016-04-08 23:21:52 -0700

    docs: Added additional comments

--- script/script.js
+++ script/script.js
@@ -1,20 +1,22 @@
 /* Declare global variable and set initial state */
 var menuState = false;
 
+  /* Shows or hides menu when toggled */
   function menuToggle() {
     var menu = document.getElementById("menu");
 
     if (menuState)
         menu.style.maxHeight = "0";
     else {
-        menu.style['transition'] = 'max-height 0.5s ease-in-out';
+        menu.style["transition"] = "max-height 0.5s ease-in-out";
         menu.style.maxHeight = "500px";
     }
     menuState = !menuState;
   }
 
+  /* Checks screen media and unhides menu items if greater than 651px */
   function checkMedia() {
-    var mq = window.matchMedia("(min-width: 641px");
+    var mq = window.matchMedia("(min-width: 651px");
     var menu = document.getElementById("menu");
 
     mq.matches ? menu.style.maxHeight = "500px" : menu.style.maxHeight = "0";
